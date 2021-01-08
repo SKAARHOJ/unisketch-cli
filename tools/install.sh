9afbc98b204ab17fa48ee7aa18f18b38ed7cc78d
Buğra Ekuklu <ekuklu@icloud.com>
2021-01-08 10:44:31 +0300

    Install creates a temporary log file.

--- tools/install.sh
+++ tools/install.sh
@@ -3,3 +3,5 @@ INSTALLATION_PATH="~/unisketch-cli"
 ln -s ~/unisketch-cli/unisketch /usr/local/bin/unisketch
 ln -s ~/unisketch-cli/unisketch-update /usr/local/bin/unisketch-update
 ln -s ~/unisketch-cli/tools/blackmagic-debug-probe-usbreset /usr/local/bin/blackmagic-debug-probe-usbreset
+
+touch ~/unisketch-cli/.logfile~
